fbf2ffb59d707150171b97965e1d559c55989e6a
mennieshen <mennieshen@gmail.com>
2013-10-01 01:59:52 -0400

    hw week05 prog v1.2

---
+++ Homework/week_05_assignment_imageParticle/src/Particle.cpp
@@ -0,0 +1,9 @@
+//
+//  Particle.cpp
+//  week05_assignment_imageParticle
+//
+//  Created by MennieShen on 13/9/30.
+//
+//
+
+#include "Particle.h"
---
+++ Homework/week_05_assignment_imageParticle/src/Particle.h
@@ -0,0 +1,14 @@
+//
+//  Particle.h
+//  week05_assignment_imageParticle
+//
+//  Created by MennieShen on 13/9/30.
+//
+//
+
+#ifndef __week05_assignment_imageParticle__Particle__
+#define __week05_assignment_imageParticle__Particle__
+
+#include <iostream>
+
+#endif /* defined(__week05_assignment_imageParticle__Particle__) */
