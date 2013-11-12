6dc022708b8216304cc76919e3c9a635b8897153
mennieshen <mennieshen@gmail.com>
2013-11-12 01:43:35 -0500

    hw 11 v1.1

---
+++ Homework/week_11_OSCreceiver_02/src/testApp.cpp
@@ -0,0 +1,91 @@
+
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    
+    ofSetVerticalSync( true );
+    ofSetFrameRate( 60 );
+    ofBackground(0);
+    
+    mReceiver2.setup( 12346 );
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    checkOsc();
+}
+
+void testApp::checkOsc(){
+    while( mReceiver2.hasWaitingMessages() ){
+        ofxOscMessage m;
+        mReceiver2.getNextMessage( &m );
+        
+        string addr = m.getAddress();
+        
+        if ( addr == "/mouse/pos"){
+            
+            int xPos = m.getArgAsInt32(0);
+            int yPos = m.getArgAsInt32(1);
+            
+            mousePos.set(xPos, yPos);
+        }
+        else if ( addr == "/ball/pos") {
+            float xPos = m.getArgAsFloat(0);
+            float yPos = m.getArgAsFloat(1);
+            
+            ballPos.set(xPos, yPos);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    ofSetColor(255,255,0);
+    ofCircle(ballPos, 10);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
+}
---
+++ Homework/week_11_OSCsender/src/testApp.cpp
@@ -0,0 +1,105 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    ofSetVerticalSync( true );
+    ofSetFrameRate( 60 );
+    ofBackground(0);
+    
+    pos = ofGetWindowSize() / 2;
+    vel = ofVec2f(10,3);
+    
+    mSender.setup("localhost", 12345);
+    mSender2.setup("localhost", 12346);
+    mSender3.setup("localhost", 12347);
+    //mSender.setup("localhost", 12348);
+    
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    pos += vel;
+    
+    if (pos.x < 0 ||  pos.x > ofGetWindowWidth() * 4) {
+        vel.x *= -1;
+    }
+    
+    if (pos.y < 0 ||  pos.y > ofGetWindowHeight()) {
+        vel.y *= -1;
+    }
+    
+    ofxOscMessage m;
+    m.setAddress("/ball/pos");
+    m.addFloatArg(pos.x);
+    m.addFloatArg(pos.y);
+    
+    mSender.sendMessage( m );
+    mSender2.sendMessage( m );
+    mSender3.sendMessage( m );
+    
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    ofPushMatrix();
+    ofTranslate( -ofGetWindowWidth(), 0);
+    ofSetColor(255, 255, 0);
+    ofCircle(pos, 10);
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+    mousePos.set(x,y);
+    
+    ofxOscMessage m;
+    m.setAddress("/mouse/pos");
+    m.addIntArg(mousePos.x);
+    m.addIntArg(mousePos.y);
+    
+    mSender.sendMessage( m );
+    mSender2.sendMessage( m );
+    mSender3.sendMessage( m );
+    
+    
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){
+    
+}
